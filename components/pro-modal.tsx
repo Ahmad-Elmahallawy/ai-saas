093e1f8ad3275d9bd616dd1306d383a15aaed31c
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2023-08-19 16:10:15 +0300

    integrated stripe into the project

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { useProModal } from "@/app/hooks/use-pro-modal";
 import {
   Dialog,
@@ -23,6 +24,8 @@ import { Badge } from "./ui/badge";
 import { Card } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import { log } from "console";
+import { useState } from "react";
 
 const tools = [
   {
@@ -59,6 +62,20 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error: any) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
@@ -91,7 +108,7 @@ export const ProModal = () => {
             </DialogDescription>
           </DialogHeader>
           <DialogFooter>
-            <Button size="lg" variant="premium" className="w-full">
+            <Button onClick={onSubscribe} size="lg" variant="premium" className="w-full">
               Upgrade <Zap className="w-4 h-4 ml-2 fill-white" />
             </Button>
           </DialogFooter>
